ff0f0910252a8c723b19e9e529c56cacb1654756
Steve Park <stevepark@Steveui-iMac.local>
2020-03-24 23:07:41 +0900

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyquibase',
+    name='pyPractice',
     version='1.0',
-    description='Python wrapper for liquibase',
-    author='Eun Woo Song',
-    author_email='songew@gmail.com',
-    url='https://github.com/rampart81/pyquibase',
+    description='Python practice',
+    author='ks steve',
+    author_email='pkssteve@gmail.com',
+    url='https://github.com/pkssteve/pyPractice',
     download_url='',
     install_requires=[],
     packages=find_packages(exclude=['docs', 'tests*']),
